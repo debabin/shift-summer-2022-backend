696637c165c0832569e9f7aa5979f999889506ae
hwapedro <dmitriybabinup@gmail.com>
2022-06-30 00:58:30 +0700

    ☄ change port

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ server.post("/api/create/order", async (req, res) => {
   );
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port 3000`);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
